be229d99696710139926b27d9371fff52646f6df
Clint <cdunn6754@gmail.com>
2019-07-21 13:47:35 -0600

    xmobar

--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,32 @@
+import System.IO
+import System.Exit
 import XMonad
-import qualified XMonad.StackSet as W
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Spiral
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ThreeColumns
+import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig
+import Graphics.X11.ExtraTypes.XF86
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
 
-main = do xmonad $ defaultConfig {
-     borderWidth = myBorderWidth,
-     focusedBorderColor = myFocusedBorderColor
-        } `additionalKeysP` myKeys
-	
+main = do
+    xmproc <- spawnPipe "xmobar myXmobarrc"
+    xmonad $ myConfig {
+      manageHook = manageDocks <+> manageHook defaultConfig
+      , layoutHook = avoidStruts $ layoutHook defaultConfig
+      , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+    }
+
 myKeys =
     [
     -- other additional keys
@@ -16,5 +36,15 @@ myKeys =
          | (key, scr)  <- zip "wer" [1,0,2] -- was [0..] *** change to match your screen order ***
          , (action, mask) <- [ (W.view, "") , (W.shift, "S-")]
     ]
+
+    
+myXmobarrc = "~/.xmonad/xmobar.hs"
 myBorderWidth = 4
 myFocusedBorderColor = "#ffff00"
+myModMask = mod1Mask
+
+myConfig = defaultConfig {
+     borderWidth = myBorderWidth
+     , focusedBorderColor = myFocusedBorderColor
+     , modMask = myModMask
+     } `additionalKeysP` myKey
